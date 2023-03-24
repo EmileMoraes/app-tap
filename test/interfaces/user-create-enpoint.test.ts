b9d9a6c12ba9c11d2a02be9c6c9e7ebe2aced504
Emile Moraes <emile.moraes@thoughtworks.com>
2023-03-24 10:05:30 -0300

    test: Add test for user create endpoint.

---
+++ test/interfaces/user-create-enpoint.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import index from '../../src/interfaces/index'
+import express, { response} from "express";
+import * as bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const userCreate = {
+        name: "test",
+        cpf: "1234",
+        email: "email@g.com",
+        password: "123"
+    }
+describe("UserCreateEndpoint", () => {
+    test("Create user", async() => {
+
+        const user = await request(index).post("/api/v1/users").send(userCreate);
+        // const addUser = user.body.data;
+        // expect(addUser).toEqual(userCreate);
+        expect(response.status(201));
+
+    })
+})
