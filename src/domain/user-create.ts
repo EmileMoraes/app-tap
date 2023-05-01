dc51736947bf38110934cf59bf18fd49b33e62b1
Emile Moraes <moraesemile@gmail.com>
2023-05-01 15:13:07 -0300

    (fix): Rename user create and moved users

--- src/domain/user-create.ts
+++ src/domain/user-create.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export type UserCreate = {
-  id: string;
+  userId: string;
   name: string;
   cpf?: string;
   cnpj?: string;
--- src/interfaces/user/mapper/user-response.ts
+++ src/interfaces/user/mapper/user-response.ts
@@ -1,3 +1,3 @@
 export type UserResponse = {
-  id: string;
+  userId: string;
 };
