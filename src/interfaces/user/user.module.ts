83f7230434f4019e11f4b39f706affb4d6d1b5fa
Emile Moraes <moraesemile@gmail.com>
2023-05-01 15:15:00 -0300

    (fix): Adjust modules

--- src/interfaces/user/user.module.ts
+++ src/interfaces/user/user.module.ts
@@ -1,7 +1,13 @@
-import { UserController } from './user-controller';
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { UserCreateUseCase } from '../../application/user-create-use-case';
+import { UserController } from '../../interfaces/user/user-controller';
+import { UserDynamodbAdapter } from '../../infrastructure/user/user-dynamodb-adapter';
+import { UserRepository } from '../../domain/user-repository';
 
+@Global()
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserCreateUseCase, UserDynamodbAdapter],
+  exports: [UserCreateUseCase]
 })
 export class UserModule {}
